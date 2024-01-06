6edcb9f6f2442d72b60ae01bc934edd4055c83fc
Ismino <denise_cigel@hotmail.com>
2024-01-06 12:14:25 +0000

    update data managment

--- src/data_management.py
+++ src/data_management.py
@@ -4,13 +4,13 @@ import numpy as np
 import joblib
 
 # code taken from https://github.com/Amareteklay/heritage-housing-issues/tree/main
-@st.cache()
+@st.cache(allow_output_mutation=True, ttl=3600)
 def load_housing_data():
     df = pd.read_csv("outputs/datasets/collection/HousePrices.csv")
     return df
 
 
-@st.cache()
+@st.cache(allow_output_mutation=True, ttl=3600)
 def load_heritage_data():
     df_h = pd.read_csv("inputs/datasets/raw/house-price-20211124T154130Z-001/house-price/inherited_houses.csv")
     return df_h
